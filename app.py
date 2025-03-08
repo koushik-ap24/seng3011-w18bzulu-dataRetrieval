361d0c0fa120b39e82e8540d4fc9e829a92eb6db
Koushik Anand Pirabu <z5421830@ad.unsw.edu.au>
2025-03-08 14:30:32 +1100

    initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+from mangum import Mangum  # Required to make Flask Lambda-compatible
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify(message="Hello from Flask on AWS Lambda!")
+
+handler = Mangum(app)
