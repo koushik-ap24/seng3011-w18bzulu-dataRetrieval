84a951b9b4b41da33788fdf282c83fb30cce8ea5
angela <z5416428@ad.unsw.edu.au>
2025-03-11 00:06:29 +1100

    fixes tests to use correct file name and function name

--- tests/populationUnitTest.py
+++ tests/populationUnitTest.py
@@ -1,20 +1,21 @@
 import unittest
-from population.py import getPopulation
+from retrival.py import population
 
 class populationUnitTest(unittest.TestCase):
     def validYearRangeQuery(self):
-        result = getPopulation(2026, 2028, "Ryde")
-        self.assertGreater(len(result), 3)
+        result = population(2026, 2028, "Ryde")
+        self.assertEqual(len(result), 3)
     
     def singleYearQuery(self):
-        result = getPopulation(2022, 2022, "Burwood")
+        result = population(2022, 2022, "Burwood")
+        self.assertEqual(len(result), 1)
     
     def invalidYearRangeQuery(self):
-        result = getPopulation(2026, 2021, "Albury")
+        result = population(2026, 2021, "Albury")
         self.assertRaises(Exception)
     
     def invalidYearQuery(self):
-        result = getPopulation(1999, 2021, "Albury")
+        result = population(1999, 2021, "Albury")
         self.assertRaises(Exception)
 
 if __name__ == '__main__':
