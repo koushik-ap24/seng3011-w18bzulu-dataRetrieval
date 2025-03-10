0eeaa5adc50d4619968c5b48311942e2629a376f
angela <z5416428@ad.unsw.edu.au>
2025-03-11 00:54:37 +1100

    changes import file and invalid queries to expect none

--- tests/populationUnitTest.py
+++ tests/populationUnitTest.py
@@ -12,11 +12,11 @@ class populationUnitTest(unittest.TestCase):
     
     def invalidYearRangeQuery(self):
         result = population(2026, 2021, "Albury")
-        self.assertRaises(Exception)
+        self.assertEqual(None)
     
     def invalidYearQuery(self):
         result = population(1999, 2021, "Albury")
-        self.assertRaises(Exception)
+        self.assertEqual(None)
 
 if __name__ == '__main__':
     unittest.main()
