2fc8bbddde7ed0b8bb3999d1a68f12b97e2c857a
Sebastian Goh <sebastian.goh@student.unsw.edu.au>
2025-03-10 21:21:33 +1100

    removing mangum from files

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify
-from mangum import Mangum  # Required to make Flask Lambda-compatible
 import retrival
 
 app = Flask(__name__)
@@ -7,8 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify(message="Hello from Flask on AWS Lambda!")
-
-handler = Mangum(app)
 
 # Sample change
 @app.get("/population/v1")
