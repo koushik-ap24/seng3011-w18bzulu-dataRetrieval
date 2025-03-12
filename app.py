11ea9c2dc298dcc67e92f0f0cec61c7cc1c7749a
Koushik Anand Pirabu <z5421830@ad.unsw.edu.au>
2025-03-12 12:05:21 +1100

    revert yml to only trigger when merged into main

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask, jsonify
 import awsgi
-import sys
-import os
-
-# Only modify path in Lambda environment
-if os.environ.get('AWS_EXECUTION_ENV') is not None:
-    sys.path.append("/opt")  # AWS Lambda mounts layers under /opt
 
 app = Flask(__name__)
 
