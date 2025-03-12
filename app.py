bd7be8b7582b61d93482597aba021bf8bc71180c
Koushik Anand Pirabu <z5421830@ad.unsw.edu.au>
2025-03-12 11:07:25 +1100

    use aws-wsgi instead of mangum

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask, jsonify
 from mangum import Mangum  # Required to make Flask Lambda-compatible
+import awsgi
+import sys
+import os
+
+# Only modify path in Lambda environment
+if os.environ.get('AWS_EXECUTION_ENV') is not None:
+    sys.path.append(os.path.join(os.path.dirname(__file__), "dependencies"))
 
 app = Flask(__name__)
 
@@ -7,4 +14,8 @@ app = Flask(__name__)
 def home():
     return jsonify(message="Hello from Flask on AWS Lambda!")
 
-handler = Mangum(app, lifespan="off")
+def lambda_handler(event, context):
+    return awsgi.response(app, event, context, base64_content_types={"image/png"})
+
+if __name__ == "__main__":
+    app.run()
