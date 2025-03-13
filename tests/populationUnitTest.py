e37d884370c515d334fa33da7e3759cd021b8767
angela <z5416428@ad.unsw.edu.au>
2025-03-13 21:49:51 +1100

    adds amended unit testing for population retrieval

--- tests/populationUnitTest.py
+++ tests/populationUnitTest.py
@@ -1,22 +1,53 @@
 import unittest
+import json
 from retrival.py import population
 
 class populationUnitTest(unittest.TestCase):
     def validYearRangeQuery(self):
-        result = population(2026, 2028, "Ryde")
-        self.assertEqual(len(result), 3)
+        jsonResult = population(2026, 2028, "Ryde")
+        result = json.loads(jsonResult)
+        estArray = result["suburbPopulationEstimate"]
+        yearArray = result["year"]
+        self.assertEqual(len(estArray), 3)
+        self.assertListEqual([2026, 2027, 2028], yearArray)
     
     def singleYearQuery(self):
-        result = population(2022, 2022, "Burwood")
-        self.assertEqual(len(result), 1)
+        jsonResult = population(2022, 2022, "Burwood")
+        result = json.loads(jsonResult)
+        estArray = result["suburbPopulationEstimate"]
+        yearArray = result["year"]
+        self.assertEqual(len(estArray), 1)
+        self.assertListEqual([2022], yearArray)
     
-    def invalidYearRangeQuery(self):
-        result = population(2026, 2021, "Albury")
-        self.assertEqual(result, None)
+    def invalidEndYear(self):
+        jsonResult = population(2021, 2088, "Albury")
+        result = json.loads(jsonResult)
+        err = result["Error"]
+        self.assertEquals("Invalid end year", err)
     
-    def invalidYearQuery(self):
-        result = population(1999, 2021, "Albury")
-        self.assertEqual(result, None)
+    def invalidStartYear(self):
+        jsonResult = population(1999, 2023, "Albury")
+        result = json.loads(jsonResult)
+        err = result["Error"]
+        self.assertEquals("Invalid start year", err)
+
+    def invalidYearRange(self):
+        jsonResult = population(2026, 2040, "Albury")
+        result = json.loads(jsonResult)
+        err = result["Error"]
+        self.assertEquals("Invalid year range", err)
+    
+    def invalidSuburb(self):
+        jsonResult = population(2021, 2022, "Auburn")
+        result = json.loads(jsonResult)
+        err = result["Error"]
+        self.assertEquals("DB does not have data for all suburbs", err)
+    
+    def emptySuburb(self):
+        jsonResult = population(2021, 2022, "")
+        result = json.loads(jsonResult)
+        err = result["Error"]
+        self.assertEquals("No suburb found", err)
 
 if __name__ == '__main__':
     unittest.main()
