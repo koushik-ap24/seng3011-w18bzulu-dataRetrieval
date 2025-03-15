0343a443186383ea954ba7cc8d788a8ac97fd433
angela <z5416428@ad.unsw.edu.au>
2025-03-15 18:09:09 +1100

    amends unit tests

--- tests/populationUnitTest.py
+++ tests/populationUnitTest.py
@@ -1,9 +1,14 @@
 import unittest
 import json
-from retrival.py import population
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+from retrival import population
 
 class populationUnitTest(unittest.TestCase):
-    def validYearRangeQuery(self):
+    def testValidYearRangeQuery(self):
         jsonResult = population(2026, 2028, "Ryde")
         result = json.loads(jsonResult)
         estArray = result["suburbPopulationEstimate"]
@@ -11,7 +16,7 @@ class populationUnitTest(unittest.TestCase):
         self.assertEqual(len(estArray), 3)
         self.assertListEqual([2026, 2027, 2028], yearArray)
     
-    def singleYearQuery(self):
+    def testSingleYearQuery(self):
         jsonResult = population(2022, 2022, "Burwood")
         result = json.loads(jsonResult)
         estArray = result["suburbPopulationEstimate"]
@@ -19,7 +24,7 @@ class populationUnitTest(unittest.TestCase):
         self.assertEqual(len(estArray), 1)
         self.assertListEqual([2022], yearArray)
 
-    def validMissingYearsQuery(self):
+    def testValidMissingYearsQuery(self):
         jsonResult = population(2033, 2041, "Burwood")
         result = json.loads(jsonResult)
         estArray = result["suburbPopulationEstimate"]
@@ -27,37 +32,37 @@ class populationUnitTest(unittest.TestCase):
         self.assertEqual(len(estArray), 2)
         self.assertListEqual([2036, 2041], yearArray)
     
-    def invalidEndYear(self):
+    def testInvalidEndYear(self):
         jsonResult = population(2021, 2088, "Albury")
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("Invalid end year", err)
     
-    def invalidStartYear(self):
+    def testInvalidStartYear(self):
         jsonResult = population(1999, 2023, "Albury")
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("Invalid start year", err)
 
-    def invalidYearRange(self):
+    def testInvalidYearRange(self):
         jsonResult = population(2042, 2044, "Albury")
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("Invalid year range", err)
 
-    def invalidYearOrder(self):
+    def testInvalidYearOrder(self):
         jsonResult = population(2030, 2021, "Albury")
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("Start year is greater than end year", err)
     
-    def invalidSuburb(self):
+    def testInvalidSuburb(self):
         jsonResult = population(2021, 2022, "A")
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("No suburb found", err)
     
-    def emptySuburb(self):
+    def testEmptySuburb(self):
         jsonResult = population(2021, 2022, "")
         result = json.loads(jsonResult)
         err = result["Error"]
--- tests/populationsUnitTest.py
+++ tests/populationsUnitTest.py
@@ -1,9 +1,14 @@
 import unittest
 import json
-from retrival.py import populations
+import sys
+import os
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(parent_dir)
+
+from retrival import populations
 
 class populationsUnitTest(unittest.TestCase):
-    def validYearRangeQuery(self):
+    def testValidYearRangeQuery(self):
         suburbs = ["Ryde", "Albury", "Strathfield"]
         jsonResult = populations(2021, 2025, "ERP", suburbs)
         result = json.loads(jsonResult)
@@ -14,7 +19,7 @@ class populationsUnitTest(unittest.TestCase):
             self.assertListEqual(suburb["years"], [2021, 2022, 2023, 2024, 2025])
             self.assertIn(suburb["suburb"], suburbs)
     
-    def singleYearQuery(self):
+    def testSingleYearQuery(self):
         suburbs = ["Burwood", "Ryde", "Albury", "Strathfield"]
         jsonResult = populations(2022, 2022, "ERP", suburbs)
         result = json.loads(jsonResult)
@@ -25,7 +30,7 @@ class populationsUnitTest(unittest.TestCase):
             self.assertListEqual(suburb["years"], [2022])
             self.assertIn(suburb["suburb"], suburbs)
     
-    def validMissingYearsQuery(self):
+    def testValidMissingYearsQuery(self):
         suburbs = ["Burwood", "Strathfield"]
         jsonResult = populations(2055, 2057, "ERP", suburbs)
         result = json.loads(jsonResult)
@@ -37,37 +42,37 @@ class populationsUnitTest(unittest.TestCase):
             self.assertIn(suburb["suburb"], suburbs)
 
         
-    def invalidStartYear(self):
+    def testInvalidStartYear(self):
         jsonResult = populations(1998, 2026, "ERP", ["Ryde", "Albury", "Strathfield"])
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("Invalid start year", err)
 
-    def invalidEndYear(self):
+    def testInvalidEndYear(self):
         jsonResult = populations(2025, 2191, "ERP", ["Ryde", "Albury", "Strathfield"])
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("Invalid end year", err)
 
-    def invalidYearOrder(self):
+    def testInvalidYearOrder(self):
         jsonResult = populations(2027, 2025, "ERP", ["Ryde", "Albury", "Strathfield"])
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("Start year is greater than end year", err)
     
-    def invalidYearRange(self):
+    def testInvalidYearRange(self):
         jsonResult = populations(2033, 2034, "ERP", ["Ryde", "Albury", "Strathfield"])
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("Invalid year range", err)
 
-    def invalidSuburbs(self):
+    def testInvalidSuburbs(self):
         jsonResult = populations(2023, 2027, "ERP", ["A", "B", "C"])
         result = json.loads(jsonResult)
         err = result["Error"]
         self.assertEquals("No suburb found", err)
 
-    def missingSuburb(self):
+    def testMissingSuburb(self):
         jsonResult = populations(2023, 2027, "ERP", ["Albury", "B"])
         result = json.loads(jsonResult)
         err = result["Error"]
