eb7ac6bf1fabadb3864c3fe1bbcc5d58bcc0e85f
Sebastian Goh <sebastian.goh@student.unsw.edu.au>
2025-03-18 14:52:08 +1100

    copying over population tests

--- tests/test_population.py
+++ tests/test_population.py
@@ -13,7 +13,7 @@ except ImportError:
     pytest.skip(reason="could not import population", allow_module_level=True)
 
 
-class TestPopulation:
+class TestPopulation():
     def helperValidQuery(self, start, end, suburb, expectedEst, expectedYears):
         jsonResult = population(start, end, suburb)
         result = json.loads(jsonResult)
@@ -28,17 +28,17 @@ class TestPopulation:
         assert errMsg == result["error"]
 
     def testValidYearRangeQuery(self):
-        self.helperValidQuery(2026, 2028, "Ryde", 3, [2026, 2027, 2028])
+       self.helperValidQuery(2026, 2028, "Ryde", 3, [2026, 2027, 2028])
 
     def testSingleYearQuery(self):
         self.helperValidQuery(2022, 2022, "Burwood", 1, [2022])
 
     def testValidMissingYearsQuery(self):
-        self.helperValidQuery(2026, 2028, "Burwood", 2, [2036, 2041])
-
+        self.helperValidQuery(2032, 2044, "Burwood", 2, [2036, 2041])
+    
     def testInvalidEndYear(self):
         self.helperInvalidQuery(2021, 2088, "Albury", "Invalid end year")
-
+    
     def testInvalidStartYear(self):
         self.helperInvalidQuery(1999, 2023, "Albury", "Invalid start year")
 
@@ -46,12 +46,10 @@ class TestPopulation:
         self.helperInvalidQuery(2042, 2044, "Albury", "Invalid year range")
 
     def testInvalidYearOrder(self):
-        self.helperInvalidQuery(
-            2030, 2021, "Albury", "Start year is greater than end year"
-        )
-
+        self.helperInvalidQuery(2030, 2021, "Albury", "Start year is greater than end year")
+    
     def testInvalidSuburb(self):
         self.helperInvalidQuery(2021, 2022, "A", "No suburb found")
-
+    
     def testEmptySuburb(self):
         self.helperInvalidQuery(2021, 2022, "", "No suburb found")
